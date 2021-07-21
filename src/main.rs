7f98582bc8c5c2f59054b2af96e0ce2e13dbe21c
Agorgianitis Loukas <agorglouk@gmail.com>
2021-07-22 02:00:56 +0300

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate log;
+
 fn main() {
-    println!("Hello, world!");
+    let log_env = env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
+    env_logger::init_from_env(log_env);
+    log::info!("Hello there!");
 }
