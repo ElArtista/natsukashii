e979132cd500dff512bf8afd5d7d27a44f76daa0
Agorgianitis Loukas <agorglouk@gmail.com>
2021-07-22 10:13:41 +0300

    Setup graphics

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
 extern crate log;
 
+mod gfx;
+
+use gfx::Gfx;
 use winit::{
     event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
+
+fn render(gfx: &Gfx) {
+    let frame = gfx
+        .swapchain
+        .get_current_frame()
+        .expect("Failed to acquire next swap chain texture")
+        .output;
+
+    let mut encoder = gfx
+        .device
+        .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+    {
+        let _rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[wgpu::RenderPassColorAttachment {
+                view: &frame.view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
+                    store: true,
+                },
+            }],
+            depth_stencil_attachment: None,
+        });
+    }
+
+    gfx.queue.submit(Some(encoder.finish()));
+}
 
 fn run() {
     // Create window and its event loop
@@ -15,12 +46,17 @@ fn run() {
         .build(&event_loop)
         .unwrap();
 
+    // Setup graphics
+    let mut gfx = futures::executor::block_on(Gfx::new(&window));
+
     // Run window even loop
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
             Event::WindowEvent { event, window_id } if window_id == window.id() => match event {
+                // Handle resize event
+                WindowEvent::Resized(size) => gfx.configure_swapchain(size.into()),
                 // Handle window close event
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
                 // Handle key escape event
@@ -34,6 +70,8 @@ fn run() {
                 },
                 _ => (),
             },
+            // Handle redraw event
+            Event::RedrawRequested(_) => render(&gfx),
             // Handle events processed event
             Event::MainEventsCleared => window.request_redraw(),
             _ => (),
