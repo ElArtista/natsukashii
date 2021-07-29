b1ec6d8ddda78e1b5e6235224c08d875365b6129
Agorgianitis Loukas <agorglouk@gmail.com>
2021-07-29 23:28:38 +0300

    Refactor high level functionality to engine module

---
+++ src/engine.rs
@@ -0,0 +1,161 @@
+use super::renderer::{DefaultRenderer, Renderer, RendererScene};
+use winit::{
+    dpi::PhysicalSize,
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+    window::WindowBuilder,
+};
+
+/// Initialization parameters for Engine
+pub struct EngineParams {
+    pub window: WindowParams,
+}
+
+/// Initialization parameters for Window
+pub struct WindowParams {
+    pub size: (u32, u32),
+}
+
+/// The Engine
+///
+/// Manages initialization, lifetime and plumbing of
+/// the window, the event loop and the renderer
+pub struct Engine {
+    pub event_loop: Option<EventLoop<()>>,
+    pub window: Window,
+    pub instance: wgpu::Instance,
+    pub surface: wgpu::Surface,
+    pub adapter: wgpu::Adapter,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub swapchain: wgpu::SwapChain,
+    pub swapchain_desc: wgpu::SwapChainDescriptor,
+    pub renderer: DefaultRenderer,
+}
+
+impl Engine {
+    pub async fn new(params: &EngineParams) -> Self {
+        // Create window and its event loop
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new()
+            .with_inner_size(PhysicalSize::<u32>::from(params.window.size))
+            .with_resizable(false)
+            .build(&event_loop)
+            .unwrap();
+
+        // Create wgpu instance, surface and adapter
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY | wgpu::BackendBit::SECONDARY);
+        let surface = unsafe { instance.create_surface(&window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                // Request an adapter which can render to our surface
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .expect("Failed to find an appropriate adapter");
+
+        // Create the logical device and command queue
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None,
+            )
+            .await
+            .expect("Failed to create device");
+
+        // Create swapchain
+        let size = window.inner_size();
+        let swapchain_format = adapter.get_swap_chain_preferred_format(&surface).unwrap();
+        let swapchain_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format: swapchain_format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Mailbox,
+        };
+        let swapchain = device.create_swap_chain(&surface, &swapchain_desc);
+
+        // Create the renderer
+        let renderer = DefaultRenderer::new(&device, &swapchain_desc);
+
+        // Store objects
+        Self {
+            event_loop: Some(event_loop),
+            window,
+            instance,
+            surface,
+            adapter,
+            device,
+            queue,
+            swapchain,
+            swapchain_desc,
+            renderer,
+        }
+    }
+
+    pub fn resize(&mut self, size: (u32, u32)) {
+        // Recreate the swap chain with the new size
+        self.swapchain_desc.width = size.0;
+        self.swapchain_desc.height = size.1;
+        self.swapchain = self
+            .device
+            .create_swap_chain(&self.surface, &self.swapchain_desc);
+    }
+
+    pub fn render(&self) {
+        // Acquire frame
+        let (swapchain, device, queue) = (&self.swapchain, &self.device, &self.queue);
+        let frame = swapchain
+            .get_current_frame()
+            .expect("Failed to acquire next swap chain texture")
+            .output;
+
+        // Create encoder
+        let encoder_desc = wgpu::CommandEncoderDescriptor { label: None };
+        let mut encoder = device.create_command_encoder(&encoder_desc);
+
+        // Render and submit the queue
+        self.renderer.render(&mut encoder, queue, &frame.view);
+        queue.submit(Some(encoder.finish()));
+    }
+
+    pub fn run(mut self) {
+        // Workaround the static lifetime requirements of event_loop
+        let event_loop = self.event_loop.take().unwrap();
+
+        // Run the mainloop
+        event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Wait;
+            match event {
+                Event::WindowEvent { event, window_id } if window_id == self.window.id() => {
+                    match event {
+                        WindowEvent::Resized(size) => self.resize(size.into()),
+                        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                        WindowEvent::KeyboardInput { input, .. } => match input {
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                            _ => (),
+                        },
+                        _ => (),
+                    }
+                }
+                Event::RedrawRequested(_) => self.render(),
+                Event::MainEventsCleared => self.window.request_redraw(),
+                _ => (),
+            }
+        });
+    }
+
+    pub fn set_scene(&mut self, scene: RendererScene) {
+        self.renderer.set_scene(scene);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,15 @@ extern crate log;
 
 #[macro_use]
 mod shader;
-mod gfx;
+mod engine;
 mod mesh;
 mod renderer;
 mod uniform;
 
-use gfx::Gfx;
+use engine::{Engine, EngineParams, WindowParams};
 use glam::{Mat4, Vec3};
 use mesh::{Index, Mesh, Vertex};
-use renderer::{DefaultRenderer, Renderer, RendererScene};
-use winit::{
-    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use renderer::RendererScene;
 
 #[allow(dead_code)]
 fn demo_mesh() -> Mesh {
@@ -39,90 +34,35 @@ fn demo_mesh() -> Mesh {
     Mesh { vertices, indices }
 }
 
-fn render(gfx: &Gfx, renderer: &dyn Renderer) {
-    // Acquire frame
-    let (swapchain, device, queue) = (&gfx.swapchain, &gfx.device, &gfx.queue);
-    let frame = swapchain
-        .get_current_frame()
-        .expect("Failed to acquire next swap chain texture")
-        .output;
-
-    // Create encoder
-    let encoder_desc = wgpu::CommandEncoderDescriptor { label: None };
-    let mut encoder = device.create_command_encoder(&encoder_desc);
-
-    // Render and submit the queue
-    renderer.render(&mut encoder, queue, &frame.view);
-    queue.submit(Some(encoder.finish()));
-}
-
-fn run() {
-    // Create window and its event loop
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_inner_size(winit::dpi::PhysicalSize::new(1280, 720))
-        .with_resizable(false)
-        .build(&event_loop)
-        .unwrap();
-
-    // Setup graphics
-    let mut gfx = futures::executor::block_on(Gfx::new(&window));
-
-    // Setup renderer
-    let mut renderer = DefaultRenderer::new(&gfx.device, &gfx.swapchain_desc);
-
-    // Create demo mesh and buffers
-    let mesh = demo_mesh();
-    let buffers = mesh.create_buffers(&gfx.device);
-
-    // Create demo scene
-    renderer.set_scene(RendererScene {
-        meshes: vec![buffers],
-        view: Mat4::look_at_rh((0.0, 0.0, 4.0).into(), Vec3::ZERO, Vec3::Y),
-        proj: Mat4::perspective_rh_gl(
-            (45.0 as f32).to_radians(),
-            gfx.swapchain_desc.width as f32 / gfx.swapchain_desc.height as f32,
-            0.1,
-            100.0,
-        ),
-    });
-
-    // Run window even loop
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent { event, window_id } if window_id == window.id() => match event {
-                // Handle resize event
-                WindowEvent::Resized(size) => gfx.configure_swapchain(size.into()),
-                // Handle window close event
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                // Handle key escape event
-                WindowEvent::KeyboardInput { input, .. } => match input {
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    } => *control_flow = ControlFlow::Exit,
-                    _ => (),
-                },
-                _ => (),
-            },
-            // Handle redraw event
-            Event::RedrawRequested(_) => render(&gfx, &renderer),
-            // Handle events processed event
-            Event::MainEventsCleared => window.request_redraw(),
-            _ => (),
-        }
-    });
-}
-
 fn main() {
     // Initialize logging
     let log_env = env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
     env_logger::init_from_env(log_env);
     log::info!("Hello there!");
 
-    // Run real entrypoint
-    run();
+    // Prepare the engine params
+    let params = EngineParams {
+        window: WindowParams { size: (1280, 720) },
+    };
+
+    // Create the engine
+    let mut engine = futures::executor::block_on(Engine::new(&params));
+
+    // Create demo mesh and buffers
+    let mesh = demo_mesh();
+    let buffers = mesh.create_buffers(&engine.device);
+
+    // Create demo scene
+    let aspect = params.window.size.0 as f32 / params.window.size.1 as f32;
+    let view = Mat4::look_at_rh((0.0, 0.0, 4.0).into(), Vec3::ZERO, Vec3::Y);
+    let proj = Mat4::perspective_rh_gl((45.0 as f32).to_radians(), aspect, 0.1, 100.0);
+    let scene = RendererScene {
+        meshes: vec![buffers],
+        view,
+        proj,
+    };
+    engine.set_scene(scene);
+
+    // Run
+    engine.run();
 }
